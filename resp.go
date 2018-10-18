b66af836904ca0446ea8fa01bbfdf825698c7702
sunlin <shulin10000@163.com>
2018-10-18 17:52:45 +0800

    增加解析微信通知结果

--- resp.go
+++ resp.go
@@ -94,3 +94,50 @@ func ParseQueryOrderResult(resp []byte) (QueryOrderResult, error) {
 
 	return queryOrderResult, nil
 }
+
+//微信支付异步通知服务器结果
+type NoticeOrderResult struct {
+	XMLName       xml.Name `xml:"xml"`
+	ReturnCode    string   `xml:"return_code"`
+	ReturnMsg     string   `xml:"return_msg"`
+	AppId         string   `xml:"appid"`
+	MchId         string   `xml:"mch_id"`
+	DeviceInfo    string   `xml:"device_info"`
+	NonceStr      string   `xml:"nonce_str"`
+	Sign          string   `xml:"sign"`
+	ResultCode    string   `xml:"result_code"`
+	ErrCode       string   `xml:"err_code"`
+	ErrCodeDesc   string   `xml:"err_code_des"`
+	OpenId        string   `xml:"openid"`
+	IsSubscribe   string   `xml:"is_subscribe"`
+	TradeType     string   `xml:"trade_type"`
+	BankType      string   `xml:"bank_type"`
+	TotalFee      string   `xml:"total_fee"`
+	FeeType       string   `xml:"fee_type"`
+	CashFee       string   `xml:"cash_fee"`
+	CashFeeType   string   `xml:"cash_fee_type"`
+	CouponFee     string   `xml:"coupon_fee"`
+	CouponCount   string   `xml:"coupon_count"`
+	TransactionId string   `xml:"transaction_id"`
+	OutTradeNo    string   `xml:"out_trade_no"`
+	Attach        string   `xml:"attach"`
+	TimeEnd       string   `xml:"time_end"`
+}
+
+func (this *NoticeOrderResult) ToMap() map[string]string {
+	retMap, err := ToMap(this)
+	if err != nil {
+		panic(err)
+	}
+
+	return retMap
+}
+func ParseNoticeOrderResult(resp []byte) (NoticeOrderResult, error) {
+	noticeOrderResult := NoticeOrderResult{}
+	err := xml.Unmarshal(resp, &noticeOrderResult)
+	if err != nil {
+		return noticeOrderResult, err
+	}
+
+	return noticeOrderResult, nil
+}
